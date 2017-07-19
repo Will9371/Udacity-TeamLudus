b165779f354efa9496ecc0ffd95f11f92a053cd5
Will Petillo <will.petillo@gmail.com>
2017-07-18 19:30:46 -0700

    Prototype swapping mechanic

---
+++ Photo Sequencer/Assets/Scripts/OrbSelect.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script is attached to each image orb
+public class OrbSelect : MonoBehaviour 
+{
+	static bool swapEmpty;
+	public bool isOnSwap;
+	public GameObject swapPedestal, glassOrb;
+	GameObject orbClicked;
+	public Transform myPedestalLoc; 
+	Vector3 newGlassLoc;
+
+	void Start()
+	{
+		isOnSwap = false;		//true for an orb that is on swap pedestal
+		swapEmpty = true;
+		swapPedestal = TransitionManager.instance.swapPedestal;
+		glassOrb = TransitionManager.instance.glassOrb;
+	}
+
+	public void OnPointerClick()
+	{
+		orbClicked = this.gameObject;
+		if (transform.parent.gameObject != swapPedestal)
+		{											
+			myPedestalLoc = transform.parent;
+
+			swapEmpty = true;		//assume swap pedestal is empty until iteration finds one
+			foreach (GameObject orb in TransitionManager.instance.imageOrbs)	//look through each orb
+			{
+				if (orb != orbClicked && orb.GetComponent<OrbSelect>().isOnSwap)	//if an orb (but not the one selected) is on the swapPedestal
+				{
+					swapEmpty = false;
+					orb.GetComponent<OrbSelect>().MoveToPedestal(orb, orbClicked, myPedestalLoc);//move that orb to the selected regular pedestal
+				}
+			}
+			if(gameObject != glassOrb)
+				MoveToSwap();
+		}
+		else  	//otherwise, transition to image view mode
+			TransitionManager.instance.OnOrbSelect(orbClicked);
+	}
+
+	void MoveToSwap()
+	{
+		if(swapEmpty)
+		{
+			glassOrb.transform.parent = myPedestalLoc;
+			glassOrb.transform.localPosition = Vector3.zero + new Vector3 (0f, .88f, 0f);
+		}
+
+		transform.parent = swapPedestal.transform;					  //move the selected image orb to the swap pedestal
+		transform.localPosition = Vector3.zero + new Vector3 (0f, 1.15f, 0f);
+		isOnSwap = true;
+	}
+
+	void MoveToPedestal(GameObject orb, GameObject orbClicked, Transform myPedestalLoc)
+	{
+		if(orbClicked == glassOrb)
+		{
+			Vector3 newGlassLoc = new Vector3 (transform.position.x, -20f, 3f);	//move the glass orb below the viewable area
+			glassOrb.transform.position = newGlassLoc;
+		}
+			
+		orb.transform.parent = myPedestalLoc;						//move the orb that was on the Swap Pedestal to the selected pedestal
+		transform.localPosition = Vector3.zero + new Vector3 (0f, 0.88f, 0f);
+		isOnSwap = false;
+	}
+}
---
+++ Photo Sequencer/Assets/Scripts/TransitionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach this script to: EventSystem
+public class TransitionManager : MonoBehaviour 
+{
+	//***Singleton code
+	public static TransitionManager instance;
+
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+	}
+
+	void OnDestroy()
+	{
+		if (instance == this)
+			instance = null;
+	}
+	//***
+
+	public GameObject MainSceneObjects, ReverseSphere;
+	public GameObject swapPedestal, glassOrb;
+	public GameObject[] imageOrbs;
+	public Material[] image360;
+
+	public void OnOrbSelect(GameObject currSelectedOrb)	//transition from main scene to image scene
+	{
+		MainSceneObjects.SetActive(false);
+		ReverseSphere.SetActive(true);
+		ReverseSphere.GetComponent<Renderer>().material = currSelectedOrb.GetComponent<Renderer>().material;
+		//*Better: 	Move and expand the image orb until it envelops the player
+	}
+
+	public void OnOrbExit()
+	{
+		MainSceneObjects.SetActive(true);
+		ReverseSphere.SetActive(false);
+	}
+}
